0b51b76609cd628a1718fe623f69fe10cdc2fad3
KarolMaciej <k.odrobinski@gmail.com>
2021-01-31 14:12:35 +0100

    First ver add postgresql

---
+++ ReservationConferenceRooms/Conf_Room_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConfRoomAppConfig(AppConfig):
+    name = 'Conf_Room_app'
--- ReservationConferenceRooms/ReservationConferenceRooms/settings.py
+++ ReservationConferenceRooms/ReservationConferenceRooms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Conf_Room_app',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ReservationConferenceRooms.urls'
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'ReservationConferenceRooms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ConfRoom',
+        'HOSTNAME': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
